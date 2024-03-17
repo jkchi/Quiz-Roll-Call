00d7faa81e69e8bd49d642ed627c22d8ad81a706
jkchi <113892417+jkchi@users.noreply.github.com>
2024-03-17 01:19:16 -0400

    finsh homepage

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+# HomeView inherent from Django view
+from django.views import View
 
-# Create your views here.
+# Home page only serve as the redirect page 
+# Home inherent request to handle the response
+# If user not logged in redirect to login page
+# If logged in redirect to user homepage(to be done)
+class HomeView(View):
+    def get(self,request):
+        if not request.user.is_authenticated:
+            return redirect(reverse('login'))  
+        return render(request, 'home/main.html')
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+from home.views import HomeView
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.views.static import serve
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
 ]
